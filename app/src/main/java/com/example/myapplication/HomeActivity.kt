9c0dbc9ee4a86c93b05b7979eaa62cddcd9c9036
cauammedeiros <cauammedeiros@gmail.com>
2026-04-20 17:18:18 -0300

    tela inicio

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,24 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Livros de Ação
+        val listaDeAcao = listOf(
+            Livro("Teste",R.drawable.logo)
+        )
+
+        val rvAcao = findViewById<RecyclerView>(R.id.rvAcao)
+        rvAcao.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvAcao.adapter = LivroAdapter(listaDeAcao)
+
+        // Livros de Suspense
+        val listaDeSuspense = listOf(
+            Livro("Teste",R.drawable.logo_nome)
+        )
+
+        val rvSuspense = findViewById<RecyclerView>(R.id.rvSuspense)
+        rvSuspense.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvSuspense.adapter = LivroAdapter(listaDeSuspense)
+
     }
 }
