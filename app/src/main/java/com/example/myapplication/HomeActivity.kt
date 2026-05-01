4f6529e4f25256ce940ec539a2356ed9bcd18327
Francisco Quezado <franmagnoquezado@gmail.com>
2026-05-01 16:09:41 -0300

    Criação da tela de configurações, e padronização da barra de navegação nas telas perfil, biblioteca e chatbot e na home

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -13,29 +13,5 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        // Livros de Ação
-        val listaDeAcao = listOf(
-            Livro("Teste",R.drawable.logo)
-        )
-
-        val rvAcao = findViewById<RecyclerView>(R.id.rvAcao)
-        rvAcao.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        rvAcao.adapter = LivroAdapter(listaDeAcao)
-
-        // Livros de Suspense
-        val listaDeSuspense = listOf(
-            Livro("Teste",R.drawable.logo_nome)
-        )
-
-        val rvSuspense = findViewById<RecyclerView>(R.id.rvSuspense)
-        rvSuspense.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        rvSuspense.adapter = LivroAdapter(listaDeSuspense)
-
     }
 }
--- app/src/main/java/com/example/myapplication/TelaBibliotecaActivity.kt
+++ app/src/main/java/com/example/myapplication/TelaBibliotecaActivity.kt
@@ -11,7 +11,7 @@ class TelaBibliotecaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_biblioteca)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottomNavigation)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
