2c112419dca0f31db595e5cf3b7ff667d3865794
Francisco Quezado <franmagnoquezado@gmail.com>
2026-05-03 19:41:47 -0300

    Criação das telas para o ADM

---
+++ app/src/main/java/com/example/myapplication/ConfirmarExclusaoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConfirmarExclusaoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_confirmar_exclusao)
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/GerenciarLivrosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GerenciarLivrosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_gerenciar_livros)
+    }
+}
