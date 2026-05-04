f9f16799c3622db71893d1adc63d351ebabf7890
cauammedeiros <cauammedeiros@gmail.com>
2026-05-04 10:37:07 -0300

    correção tela busca filtrada + botoes de navegação na tela de inicio

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -1,8 +1,12 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,6 +21,55 @@ class HomeActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Menu de Navegação
+        val btnInicio = findViewById<LinearLayout>(R.id.nav_home)
+        val btnBiblioteca = findViewById<LinearLayout>(R.id.nav_library)
+        val btnChatbot = findViewById<LinearLayout>(R.id.nav_chatbot)
+        val btnBuscar = findViewById<LinearLayout>(R.id.nav_search)
+        val btnPerfil = findViewById<LinearLayout>(R.id.nav_profile)
+
+        btnInicio.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnBiblioteca.setOnClickListener {
+            val intent = Intent(this, TelaBibliotecaActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnChatbot.setOnClickListener {
+            val intent = Intent(this, ChatbotActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnBuscar.setOnClickListener {
+            val intent = Intent(this, BuscaActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnPerfil.setOnClickListener {
+            val intent = Intent(this, PerfilActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        // Botões de Modo de Tema
+        val btnLight = findViewById<ImageButton>(R.id.btnLightMode)
+        val btnNight = findViewById<ImageButton>(R.id.btnNightMode)
+
+        // Configuração para o Modo Claro
+        btnLight.setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            delegate.applyDayNight()
+        }
+
+        // Configuração para o Modo Escuro
+        btnNight.setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            delegate.applyDayNight()
         }
 
         // Livros de Ação
--- app/src/main/java/com/example/myapplication/TelaBibliotecaActivity.kt
+++ app/src/main/java/com/example/myapplication/TelaBibliotecaActivity.kt
@@ -1,8 +1,13 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,6 +20,56 @@ class TelaBibliotecaActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        /* Menu de Navegação
+        val btnInicio = findViewById<LinearLayout>(R.id.nav_home)
+        val btnBiblioteca = findViewById<LinearLayout>(R.id.nav_library)
+        val btnChatbot = findViewById<LinearLayout>(R.id.nav_chatbot)
+        val btnBuscar = findViewById<LinearLayout>(R.id.nav_search)
+        val btnPerfil = findViewById<LinearLayout>(R.id.nav_profile)
+
+        btnInicio.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnBiblioteca.setOnClickListener {
+            val intent = Intent(this, TelaBibliotecaActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnChatbot.setOnClickListener {
+            val intent = Intent(this, ChatbotActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnBuscar.setOnClickListener {
+            val intent = Intent(this, BuscaActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnPerfil.setOnClickListener {
+            val intent = Intent(this, PerfilActivity::class.java)
+            startActivity(intent)
+        }
+
+         */
+
+
+        // Botões de Modo de Tema
+        val btnLight = findViewById<ImageView>(R.id.btnSun)
+        val btnNight = findViewById<ImageView>(R.id.btnMoon)
+
+        // Configuração para o Modo Claro
+        btnLight.setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            delegate.applyDayNight()
+        }
+
+        // Configuração para o Modo Escuro
+        btnNight.setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            delegate.applyDayNight()
         }
     }
 }
