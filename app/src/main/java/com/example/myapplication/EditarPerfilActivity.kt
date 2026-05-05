b53d626e03fe231f31ca6394fb3eaf4e39084c37
Francisco Quezado <franmagnoquezado@gmail.com>
2026-05-05 15:27:19 -0300

    Alterações em editar Perfil

--- app/src/main/java/com/example/myapplication/EditarPerfilActivity.kt
+++ app/src/main/java/com/example/myapplication/EditarPerfilActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class EditarPerfilActivity : AppCompatActivity() {
@@ -9,8 +11,18 @@ class EditarPerfilActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editar_perfil)
 
-        // Botão Voltar
+        // Botão Voltar (Seta)
         val btnVoltar = findViewById<ImageView>(R.id.btnVoltar)
-        btnVoltar.setOnClickListener { finish() }
+        btnVoltar.setOnClickListener {
+            finish()// Retorna para a tela anterior (Perfil)
+        }
+
+        // Botão Salvar Alterações
+        val btnSalvar = findViewById<Button>(R.id.btnSalvarAlteracoes)
+        btnSalvar.setOnClickListener {
+            // Lógica de feedback para o usuário
+            Toast.makeText(this, "Alterações salvas com sucesso!", Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
