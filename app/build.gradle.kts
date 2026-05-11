ac3eaf0a91ce7abe0920f66f5a09d25c510b51a1
Francisco Quezado <franmagnoquezado@gmail.com>
2026-05-11 20:22:39 -0300

    Implementando banco de dados com Cadastro e login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.utils.IMPLEMENTATION
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
@@ -57,7 +59,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.coordinatorlayout)
-    implementation(libs.firebase.common.ktx)
+    implementation(libs.firebase.common)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
