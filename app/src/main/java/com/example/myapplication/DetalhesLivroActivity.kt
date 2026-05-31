dd94fab6335f3ce17de0d6d09009cb1494df8d5f
carlostrevia <carlosaotrevia@gmail.com>
2026-05-31 16:23:08 -0300

    RF13.3

--- app/src/main/java/com/example/myapplication/DetalhesLivroActivity.kt
+++ app/src/main/java/com/example/myapplication/DetalhesLivroActivity.kt
@@ -153,7 +153,7 @@ class DetalhesLivroActivity : AppCompatActivity() {
                 Toast.makeText(this, "Download iniciado...", Toast.LENGTH_SHORT).show()
                 dialog.dismiss()
             } else {
-                Toast.makeText(this, "Você precisa aceitar os termos para baixar.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.err_aceitar_termos), Toast.LENGTH_SHORT).show()
             }
         }
 
